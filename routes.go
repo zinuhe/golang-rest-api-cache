9afb6b769ec741cc273f63161f3580cb5af9924d
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-06-21 21:09:11 -0400

    Update routes.go

--- routes.go
+++ routes.go
@@ -1,3 +1,5 @@
 // www.restapiexample.com/golang-tutorial/how-to-set-and-get-cache-using-golang-rest-api/
 
+// go get github.com/patrickmn/go-cache
+
 apiV1.Handle("/get-cache", controller.GetCache).Methods("GET")
