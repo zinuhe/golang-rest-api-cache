2338d026d4988cd05e1796e922b1b28d4fefc50e
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-06-21 21:12:57 -0400

    Update controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -8,9 +8,8 @@ import (
     "encoding/json"
     _"reflect"
 )
- 
- 
- 
+
+
 var SaveCache = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
    var emp helper.Emp
    decoder := json.NewDecoder(r.Body)
@@ -26,4 +25,18 @@ var SaveCache = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
  
     w.WriteHeader(http.StatusOK)
     fmt.Fprintf(w, "successfully! data has been saved in cache")
+})
+
+
+var GetCache = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    data, found := helper.GetCache("emp_data")
+    if found {
+        fmt.Println(data)
+        w.WriteHeader(http.StatusOK)
+        fmt.Fprintf(w, "successfully! data has been saved in cache")
+    } else {
+        w.WriteHeader(http.StatusBadRequest)
+        fmt.Fprintf(w, "Error! Not found key into cache")
+        return
+    }
 })
