bb554d1d18dacfbcdcd4829caf9d923a58cba661
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-06-21 21:14:09 -0400

    Update helper.go

--- helper/helper.go
+++ helper/helper.go
@@ -7,7 +7,9 @@ import (
     "strings"
 )
 
+
 var Cache = cache.New(5*time.Minute, 5*time.Minute)
+
 type Emp []struct {
     ID        int    `json:"id"`
     Name string `json:"name"`
@@ -16,4 +18,14 @@ type Emp []struct {
 func SetCache(key string, emp interface{}) bool {
     Cache.Set(key, emp, cache.NoExpiration)
     return true
+}
+
+func GetCache(key string) (Emp, bool) {
+    var emp Emp
+    var found bool
+    data, found := Cache.Get(key)
+    if found {
+      emp = data.(Emp)
+    }
+    return emp, found
 }
