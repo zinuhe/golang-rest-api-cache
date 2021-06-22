c4f624314c974c759c1a23a1392d885078e917c0
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-06-21 21:19:13 -0400

    Update routes.go

--- routes.go
+++ routes.go
@@ -1,4 +1,5 @@
 // www.restapiexample.com/golang-tutorial/how-to-set-and-get-cache-using-golang-rest-api/
+// https://adityarama1210.medium.com/fast-golang-api-performance-with-in-memory-key-value-storing-cache-1b248c182bdb
 
 // go get github.com/patrickmn/go-cache
 
